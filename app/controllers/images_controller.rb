b6a6281b378a911fa1463b28ab09848f21f77385
Luke <luke.rogers1843@gmail.com>
2020-05-05 10:05:46 -0400

    create mailers and refactor api methods readme edit

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
-  before_action :set_image, only: [:show, :edit, :update, :destroy]
+  before_action :set_image, only: [:show, :edit, :update, :destroy] 
 
   # GET /images
   def index
