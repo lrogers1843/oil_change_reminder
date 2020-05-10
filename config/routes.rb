922716b8a5929728be2796fa5b8e36600a069766
Luke <luke.rogers1843@gmail.com>
2020-05-10 18:55:42 -0400

    devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users,
-  :skip => [:registrations, :sessions]
+  # devise_for :users,
+  # :skip => [:registrations, :sessions]
 
-  as user do
-  # Registrations
-  get   '/signup'   => 'users/registrations#new', as: :new_user_registration
-  post  '/signup'   => 'users/registrations#create', as: :user_registration
-  end
+  # as user do
+  # # Registrations
+  # get   '/signup'   => 'users/registrations#new', as: :new_user_registration
+  # post  '/signup'   => 'users/registrations#create', as: :user_registration
+  # end
   resources :notifications
   resources :images
   root to: "application#index"
